88389a4da2bb67383412ae80a8cd40335b441e86
DeppMeng <depp_meng@bitbucket.org>
2020-03-25 09:19:23 +0800

    added scripts

---
+++ scripts/unsup_res50_bs128_lr15e-3_4gpus_epoch200.sh
@@ -0,0 +1,11 @@
+python main_moco.py \
+    -a resnet50 \
+    --lr 0.015 \
+    --batch-size 128 \
+    --dist-url 'tcp://localhost:10001' \
+    --multiprocessing-distributed \
+    --world-size 1 \
+    --rank 0 \
+    --output-dir unsup_res50_bs128_lr15e-3_4gpus_epoch200 \
+    --resume checkpoint_0020.pth.tar\
+    data/imagenet/images/
