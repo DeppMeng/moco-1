1b5ad9771c0d1ca5054951cbf8d3aa4a3643bc78
DeppMeng <depp_meng@bitbucket.org>
2020-03-26 20:57:46 +0800

    trying dist launch

--- scripts/unsup_res50_bs128_lr15e-3_4gpus_epoch200_distributed_launch.sh
+++ scripts/unsup_res50_bs128_lr15e-3_4gpus_epoch200_distributed_launch.sh
@@ -3,6 +3,5 @@ python -m torch.distributed.launch --nproc_per_node=4 main_moco_distributed_laun
     --lr 0.015 \
     --batch-size 128 \
     --dist-url 'tcp://localhost:10001' \
-    --output-dir unsup_res50_bs128_lr15e-3_4gpus_epoch200_dist_launch \
-    --resume output/unsup_res50_bs128_lr15e-3_4gpus_epoch200/checkpoint_current.pth.tar \
+    --output-dir unsup_res50_bs128_lr15e-3_4gpus_epoch200 \
     data/imagenet/images/
