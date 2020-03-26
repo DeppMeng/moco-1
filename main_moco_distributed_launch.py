ffb64e74bcf2824bd6d71cbfee5a9a0f193e2a1c
DeppMeng <depp_meng@bitbucket.org>
2020-03-26 21:09:37 +0800

    trying dist launch

--- main_moco_distributed_launch.py
+++ main_moco_distributed_launch.py
@@ -214,6 +214,7 @@ def main_worker(gpu, ngpus_per_node, args):
     if args.resume:
         if args.resume == 'auto':
             ckpt = 'output/{}/checkpoint_current.pth'.format(args.output_dir)
+            print(ckpt)
             if os.path.isfile(ckpt):
                 logger.info("=> loading checkpoint '{}'".format(ckpt))
                 if args.gpu is None:
