bda70d5b21207159856577cc8e5346039a326466
DeppMeng <depp_meng@bitbucket.org>
2020-03-26 21:22:05 +0800

    trying dist launch

--- main_moco_distributed_launch.py
+++ main_moco_distributed_launch.py
@@ -296,8 +296,7 @@ def main_worker(gpu, ngpus_per_node, args):
         # train for one epoch
         train(train_loader, model, criterion, optimizer, epoch, logger, args)
 
-        if not args.multiprocessing_distributed or (args.multiprocessing_distributed
-                and args.rank % ngpus_per_node == 0):
+        if dist.get_rank() == 0:
             save_checkpoint({
                 'epoch': epoch + 1,
                 'arch': args.arch,
