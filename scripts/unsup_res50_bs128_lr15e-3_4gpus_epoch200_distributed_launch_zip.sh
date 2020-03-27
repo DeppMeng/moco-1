525fb5b7138512ceed8ab42e9523b9d306d27f00
DeppMeng <depp_meng@bitbucket.org>
2020-03-27 14:36:39 +0800

    trying zip dataloader

--- scripts/unsup_res50_bs128_lr15e-3_4gpus_epoch200_distributed_launch_zip.sh
+++ scripts/unsup_res50_bs128_lr15e-3_4gpus_epoch200_distributed_launch_zip.sh
@@ -5,4 +5,5 @@ python -m torch.distributed.launch --nproc_per_node=4 main_moco_distributed_laun
     --dist-url 'tcp://localhost:10001' \
     --output-dir unsup_res50_bs128_lr15e-3_4gpus_epoch200_dist_launch \
     --resume output/unsup_res50_bs128_lr15e-3_4gpus_epoch200_dist_launch/checkpoint_current.pth.tar \
+    --data-format zip \
     data/imagenet/images/
