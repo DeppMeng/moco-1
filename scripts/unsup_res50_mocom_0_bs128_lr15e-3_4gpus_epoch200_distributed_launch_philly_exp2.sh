64f4423933370fd46af6d90f37e7706e41263fce
DeppMeng <depp_meng@bitbucket.org>
2020-04-01 21:02:31 +0800

    debugging tsv data

--- scripts/unsup_res50_mocom_0_bs128_lr15e-3_4gpus_epoch200_distributed_launch_philly_exp2.sh
+++ scripts/unsup_res50_mocom_0_bs128_lr15e-3_4gpus_epoch200_distributed_launch_philly_exp2.sh
@@ -4,7 +4,7 @@ python -m torch.distributed.launch --nproc_per_node=4 main_moco_distributed_laun
     --lr 0.015 \
     --batch-size 128 \
     --dist-url 'tcp://localhost:10001' \
-    --output-dir unsup_res50_bs128_lr15e-3_4gpus_epoch200_dist_launch_philly_exp2 \
+    --output-dir unsup_res50_bs128_lr15e-3_4gpus_epoch200_dist_launch_philly_mocom-0-exp2 \
     --resume auto \
     --moco-m 0 \
     ../data/imagenet/images/
