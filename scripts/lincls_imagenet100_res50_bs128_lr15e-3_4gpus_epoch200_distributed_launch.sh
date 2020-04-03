9c2848eb1496b1079d3b14d8d37f1f000598398d
DeppMeng <depp_meng@bitbucket.org>
2020-04-03 21:56:32 +0800

    added improved lincls scripts

--- scripts/lincls_imagenet100_res50_bs128_lr15e-3_4gpus_epoch200_distributed_launch.sh
+++ scripts/lincls_imagenet100_res50_bs128_lr15e-3_4gpus_epoch200_distributed_launch.sh
@@ -3,7 +3,6 @@ python -m torch.distributed.launch --nproc_per_node=4 main_lincls_dist_launch.py
     --lr 30 \
     --batch-size 256 \
     --epochs 60 \
-    --dist-url 'tcp://localhost:10001' \
     --pretrained output/unsup_imagenet100_res50_bs128_lr15e-3_4gpus_epoch200_dist_launch/checkpoint_current.pth.tar \
     --output-dir unsup_imagenet100_res50_bs128_lr15e-3_4gpus_epoch200_dist_launch \
     --resume auto \
