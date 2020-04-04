08ed9b2ed5acf6da00730a3cc4805b1025f7dcf0
DeppMeng <depp_meng@bitbucket.org>
2020-04-04 11:27:36 +0800

    added lincls scripts

---
+++ scripts/lincls_imagenet100_res50_mocov2_bs128_lr15e-3_4gpus_epoch800_distributed_launch_aml_exp2.sh
@@ -0,0 +1,11 @@
+source activate moco-python3.6.3
+python -m torch.distributed.launch --nproc_per_node=4 main_lincls_dist_launch.py \
+    -a resnet50 \
+    --lr 30 \
+    --batch-size 256 \
+    --epochs 60 \
+    --pretrained output/unsup_imagenet100_res50_mocov2_bs128_lr15e-3_4gpus_epoch800_distributed_launch_aml_exp2/checkpoint_current.pth.tar \
+    --output-dir unsup_imagenet100_res50_mocov2_bs128_lr15e-3_4gpus_epoch800_distributed_launch_aml_exp2 \
+    --resume auto \
+    --tsv-data \
+    ../data/imagenet/images/
